fb517556f6ee161fb1206ce7b72f0246a5993be5
XinHong2021 <91378170+XinHong2021@users.noreply.github.com>
2021-10-18 08:36:07 +0800

    Add files via upload

---
+++ Assignment/Ass2.js
@@ -0,0 +1,44 @@
+var vg_3 = "Partici_map.json"; 
+
+vegaEmbed("#choropleth_map", vg_3).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_2 = "Participation_by_year.json"; 
+
+vegaEmbed("#bar_chart", vg_2).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_1 = "2years_T_parti.json"; 
+
+vegaEmbed("#line_chart", vg_1).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_4 = "Scattler_Austra.json"; 
+
+vegaEmbed("#scattler_chart", vg_4).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+
+var vg_5 = "Scatt_bar_Aus.json"; 
+
+vegaEmbed("#scattler2_chart", vg_5).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+
+var vg_6 = "2020disci.json"; 
+
+vegaEmbed("#vert_bar_chart", vg_6).then(function(result) { 
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
